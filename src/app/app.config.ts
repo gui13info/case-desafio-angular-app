8ec87a4ea3c5de94471d215b8344c43fc19f9b61
Guilherme Marques <marques.guilherme0067@gmail.com>
2025-05-18 18:20:33 -0300

    CHORE: Importando provideHttpClient no appConfig

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,10 @@
-import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { ApplicationConfig } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-    providers: [provideRouter(routes), provideAnimationsAsync()]
+    providers: [provideRouter(routes), provideAnimationsAsync(), provideHttpClient()]
 };
