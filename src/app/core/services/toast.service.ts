76c506012652897576b86cc8543a1dd2a926c5c9
Guilherme Marques <marques.guilherme0067@gmail.com>
2025-05-18 11:56:28 -0300

    Criando toastService para exibição de mensagens

---
+++ src/app/core/services/toast.service.ts
@@ -0,0 +1,31 @@
+import { effect, Injectable, signal } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+import { StyleEnum } from '../enums/toast.enum';
+import { ToastInterface } from '../interfaces/toast.interface';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ToastService {
+    private _toast = signal<ToastInterface | null>(null);
+    public readonly toast = this._toast.asReadonly();
+
+    constructor(private snackBar: MatSnackBar) {
+        effect(() => {
+            const msg = this.toast();
+            if (msg) {
+                this.snackBar.open(msg.message, '', {
+                    duration: msg.duration ?? 3000,
+                    horizontalPosition: 'right',
+                    verticalPosition: 'top',
+                    panelClass: [`toast-${msg.type}`]
+                });
+            }
+        });
+    }
+
+    public showMessage(message: string, type: StyleEnum = StyleEnum.danger, duration = 3000) {
+        this._toast.set({ message, type, duration });
+    }
+}
