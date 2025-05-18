95c214f743fe3800b8dab45ffd68784f7d101577
Guilherme Marques <marques.guilherme0067@gmail.com>
2025-05-18 11:54:37 -0300

    FEAT: Criando component de steps

---
+++ src/app/core/components/stepper/stepper.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-stepper',
+    standalone: true,
+    imports: [],
+    templateUrl: './stepper.component.html',
+    styleUrl: './stepper.component.scss'
+})
+export class StepperComponent {}
