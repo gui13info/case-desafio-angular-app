64b22c97db143965ffadf1ff8859e8e716ba2224
Guilherme Marques <marques.guilherme0067@gmail.com>
2025-05-18 20:06:55 -0300

    FEAT: Melhorando header para setar title e subtitle dinamicamente

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
-
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { Event, NavigationStart, Router } from '@angular/router';
+
+import { Subscription } from 'rxjs';
+
+import { RotasEnum } from '../../enums/rotas.enum';
 
 @Component({
     selector: 'app-header',
@@ -9,7 +13,42 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     templateUrl: './header.component.html',
     styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
     public title = 'Cooperativa de Crédito';
     public subtitle = 'Sistema de Gestão de Cooperados';
+    private routeSubscription: Subscription;
+
+    constructor(private router: Router) {}
+
+    ngOnInit(): void {
+        this.routeSubscription = this.router.events.subscribe((event: Event) => {
+            if (event instanceof NavigationStart) {
+                const rota: string = event.url.replace('/', '');
+                this.title = this.getTitle(rota);
+                this.subtitle = this.getSubtitle(rota);
+            }
+        });
+    }
+
+    ngOnDestroy(): void {
+        this.routeSubscription.unsubscribe();
+    }
+
+    public getTitle(rota: string): string {
+        const rotas = {
+            [RotasEnum.NOVO_COOPERADO]: 'NOVA ADMISSÃO COOPERADO',
+            default: 'COOPERATIVA DE CRÉDITO'
+        };
+
+        return rotas[rota] || rotas.default;
+    }
+
+    public getSubtitle(rota: string): string {
+        const rotas = {
+            [RotasEnum.NOVO_COOPERADO]: 'Cadastro / Admissão do Cooperado / Nova Admissão de Cooperado',
+            default: 'Sistema de Gestão de Cooperados'
+        };
+
+        return rotas[rota] || rotas.default;
+    }
 }
