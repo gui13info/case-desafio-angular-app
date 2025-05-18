0bfe3c9b59bae4ffd64f0a8b45ef2f698bd23ad3
Guilherme Marques <marques.guilherme0067@gmail.com>
2025-05-18 15:13:20 -0300

    CHORE: Adicionando modulo para SnackBar do angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 import { HeaderComponent } from './core/components/header/header.component';
 import { SidebarComponent } from './core/components/sidebar/sidebar.component';
@@ -7,7 +8,7 @@ import { SidebarComponent } from './core/components/sidebar/sidebar.component';
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [HeaderComponent, SidebarComponent, RouterOutlet],
+    imports: [HeaderComponent, SidebarComponent, MatSnackBarModule, RouterOutlet],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
