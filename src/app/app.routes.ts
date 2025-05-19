f42e00872a13172d04e29bb1abd0a2cccbc6ecf9
Guilherme Marques <marques.guilherme0067@gmail.com>
2025-05-18 21:11:16 -0300

    FEAT: Implementando tela de HOME

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,17 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
         path: 'novo-cooperado',
         loadChildren: () =>
             import('./feature/pages/novo-cooperado/novo-cooperado.routes').then((mod) => mod.NOVO_COOPERADO_ROUTES)
+    },
+    {
+        path: 'home',
+        loadChildren: () => import('./feature/pages/home/home.routes').then((mod) => mod.HOME_ROUTES)
     }
 ];
