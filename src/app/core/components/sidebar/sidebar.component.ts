906ff7bcf2d64541c6d2012122444a01611125aa
Guilherme Marques <marques.guilherme0067@gmail.com>
2025-05-18 21:08:06 -0300

    FEAT: Implementando Tooltip no sidebar

--- src/app/core/components/sidebar/sidebar.component.ts
+++ src/app/core/components/sidebar/sidebar.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
     selector: 'app-sidebar',
     standalone: true,
-    imports: [RouterModule, MatSidenavModule, MatListModule],
+    imports: [RouterModule, MatSidenavModule, MatListModule, MatTooltipModule],
     templateUrl: './sidebar.component.html',
     styleUrl: './sidebar.component.scss'
 })
